0cf75b50944931fcb23dcfa883cc4d46fe26dc5b
unixbob <unixbob@gmail.com>
2017-12-11 11:03:44 +0000

    Fixed the SOAP Service

--- var/www/html/quote/index.php
+++ var/www/html/quote/index.php
@@ -3,28 +3,18 @@ include ('../dbconn.php');
 $server = gethostname();
 list($first, $second, $third, $fourth, $fifth) =
     split("-", $server, 5);
-
 $selectdb = mysql_select_db("will",$con);
 $rows = mysql_query("select * from quotes");
 $number_of_rows = mysql_num_rows($rows);
-
 $linenum = (rand(10,$number_of_rows));
-
-
 $query = mysql_query("select words from quotes where id = $linenum");
-
 // Send the response
-
 $data = mysql_fetch_row($query);
 $myObj->quote = $data[0];
 $myJSON = json_encode($myObj);
-
 header('Content-type: application/json');
-
-$array1 = array('name' => 'game1',
-                'words' => $myObj);
-
+#                 'words' => $myObj);
+$array1 = array('words' => $myObj);
 echo json_encode($array1);
-
 mysql_close($con);
 ?>
